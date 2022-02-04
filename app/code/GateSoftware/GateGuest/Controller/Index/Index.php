4b956e6310aa6829099ebc63799b0cdd34eab506
kKawa <kamil.kawa@gate-software.com>
2022-02-04 23:12:26 +0100

    create basic controller

---
+++ app/code/GateSoftware/GateGuest/Controller/Index/Index.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace GateSoftware\GateGuest\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\Result\Raw;
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends Action
+{
+    public function execute()
+    {
+        /** @var Raw $result */
+        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+        $result->setContents('Hello world');
+
+        return $result;
+    }
+}
