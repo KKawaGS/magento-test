5ca00d310de379d450f442e53a6a043689cce336
kKawa <kamil.kawa@gate-software.com>
2022-07-31 23:42:13 +0200

    added on click function to toggle checkbox and change visibility property in value observable

--- app/code/GateSoftware/GateGallery/view/adminhtml/web/js/form/element/image-uploader-mixin.js
+++ app/code/GateSoftware/GateGallery/view/adminhtml/web/js/form/element/image-uploader-mixin.js
@@ -1,15 +1,22 @@
 define(function () {
     'use strict';
-
     var mixin = {
         defaults: {
-            isMultipleFiles: true
+            isMultipleFiles: true,
         },
 
         onClickCheck: function ($file) {
-            $file.visibility = $file.visibility === 'undefined' ? true : !$file.visibility;
-        }
+            const file = this.getFile((item) => item.id === $file.id);
+            const index = this.value.indexOf(file);
+            this.value()[index].visibility = !Boolean(Number($file.visibility));
+            this.value.replace(file, $file);
 
+            return true;
+        },
+
+        isVisible: function ($file) {
+            return Boolean(Number($file.visibility));
+        }
     };
 
     return function (target) {
